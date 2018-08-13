d03637914c7bb9cee96570d77b5a505970a8a4cb
dongbao <zld126126@126.com>
2018-08-13 13:26:56 +0800

    模拟数据库查询

--- src/com/demo/test/TestController.java
+++ src/com/demo/test/TestController.java
@@ -6,8 +6,12 @@
 */  
 package com.demo.test;
 
+import java.util.List;
+
 import com.alibaba.fastjson.JSONObject;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 
 /**  
  * <p>Title: TestController.java</p>  
@@ -24,6 +28,15 @@ public class TestController extends Controller{
 		jsonObject.put("status", "200");
 		jsonObject.put("data", "");
 		jsonObject.put("msg", "网络联通");
+		renderJson(jsonObject);
+	}
+	
+	public void testselect() {
+		List<Record> find = Db.find("select * from blog");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("status", "1");
+		jsonObject.put("data", find);
+		jsonObject.put("msg", "成功");
 		renderJson(jsonObject);
 	}
 }
